8128ef6fa32c0bd7ccad27c17c2e4a90d0499fd5
CJ Hartman <cjhartmanx@gmail.com>
2020-01-23 21:38:33 -0700

    animate some text

--- script.js
+++ script.js
@@ -4,22 +4,45 @@ document.addEventListener("DOMContentLoaded", function(event) {
     let bio = document.getElementById('showBio');
     let hiddenBio = document.getElementById("hiddenBio");
     bio.addEventListener('mouseenter', e => {
-        hiddenBio.style.display = 'block';
+        hiddenBio.style.opacity = '1';
+        hiddenBio.style.transition = '.3s';
+        hiddenBio.style.transitionTimingFunction = 'ease-in';
+        hiddenBio.style.transform = 'translateY(10%)';
     });
     
     bio.addEventListener('mouseleave', e => {
-        hiddenBio.style.display = 'none';
+        hiddenBio.style.transitionTimingFunction = 'ease-in';
+        hiddenBio.style.transform = 'translateY(-10%)';
+        hiddenBio.style.opacity = '0';
     });
 
     //For the work side of things
     let work = document.getElementById('showWork');
     let hiddenWork = document.getElementById("hiddenWork");
     work.addEventListener('mouseenter', e => {
-        hiddenWork.style.display = 'block';
+        hiddenWork.style.opacity = '1';
+        hiddenWork.style.transition = '.3s';
+        hiddenWork.style.transitionTimingFunction = 'ease-in';
+        hiddenWork.style.transform = 'translateY(10%)';
     });
     
     work.addEventListener('mouseleave', e => {
-        hiddenWork.style.display = 'none';
+        hiddenWork.style.transitionTimingFunction = 'ease-in';
+        hiddenWork.style.transform = 'translateY(-10%)';
+        hiddenWork.style.opacity = '0';
     });
+
+    // stop scrolling on home page
+    function preventDefault(e) {
+        e = e || window.event;
+        if (e.preventDefault)
+          e.preventDefault();
+        e.returnValue = false;  
+      }
+      
+      document.getElementById('home').onmousewheel = function(e) { 
+        document.getElementById('home').scrollTop -= e. wheelDeltaY; 
+        preventDefault(e);
+      }
     
 });
